eb9a4ef0d78a6006013a91baf9528523f9bdd956
Elad Tal <eladtal14@gmail.com>
2020-11-30 18:00:36 +0200

    render modal ready to insert more data

--- js/portfolio-controller.js
+++ js/portfolio-controller.js
@@ -11,13 +11,13 @@ function renderProjects() {
   projects.forEach(function (proj, idx) {
     strHTMLs += `
     <div class="col-md-4 col-sm-6 portfolio-item">
-      <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${idx+1}">
+      <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1" onclick="renderModal('${proj.id}')">
       <div class="portfolio-hover">
         <div class="portfolio-hover-content">
           <i class="fa fa-plus fa-3x"></i>
         </div>
       </div>
-      <img class="img-fluid" src="img/portfolio/0${idx+1}-thumbnail.jpg" alt="">
+      <img class="img-fluid" src="img/portfolio/mineSweeper.jpg" alt="">
     </a>
     <div class="portfolio-caption">
       <h4>${proj.name}</h4>
@@ -25,9 +25,48 @@ function renderProjects() {
     </div> 
     </div>`
   })
-  console.log(strHTMLs);
-  // var $elRow = $('.all-projects');
-  var elRow = document.querySelector('.all-projects');
-  elRow.innerHTML = strHTMLs
-  // $elRow.text(`${strHTMLs}`)
+  var $elRow = $('.all-projects');
+  $elRow.html(`${strHTMLs}`);
+  // console.log(strHTMLs);
+}
+
+function renderModal(projId) {
+  var proj = getProjById(projId)
+  console.log(proj);
+  var modal = `
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <!-- Project Details Go Here -->
+              <h2>${proj.name}</h2>
+              <p class="item-intro text-muted">${proj.smallName}</p>
+              <img class="img-fluid d-block mx-auto" src="${proj.imgUrl}" alt="">
+              <p>${proj.desc}</p>
+              <ul class="list-inline">
+              <li>Date: ${proj.date}</li>
+              <li>labels: ${proj.labels}</li>
+              <a href="${proj.git}" class="btn btn-info" role="button">Check it Out </a>
+              </ul>
+              <button class="btn btn-primary" data-dismiss="modal" type="button">
+                <i class="fa fa-times"></i>
+                Close Project</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`
+  // console.log(modal);
+  var $elModal = $('#portfolioModal1')
+  $elModal.html(modal);
+  // console.log($elModal);
 }
--- js/portfolio-service.js
+++ js/portfolio-service.js
@@ -4,25 +4,43 @@ var gProjects;
 
 function createProjects() {
   gProjects = []
-  gProjects.push(createProject('1', 'Mine Sweeper', 'my first project'))
   // console.log(gProjects);
+  gProjects.push(createProject('1', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/', 'Mine Sweeper game that we built as our first project in Coding Academy', '20/11/20',
+    'Matrixes, LocalStorage'))
+  gProjects.push(createProject('2', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/'))
+  gProjects.push(createProject('3', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/'))
+  gProjects.push(createProject('4', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/'))
+  gProjects.push(createProject('5', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/'))
+  gProjects.push(createProject('6', 'Mine Sweeper', 'my first project', 'img/portfolio/mineSweeper.jpg',
+    'https://eladtal14.github.io/Mine-Sweeper/'))
 }
 
-function createProject(id, name, smallName, imgUrl, desc, date, client, category) {
+function createProject(id, name, smallName, imgUrl, git, desc, date, labels) {
   var proj = {
-    id: id,
-    name: name,
+    id,
+    name,
     smallName,
     imgUrl,
+    git,
     desc,
     date,
-    client,
-    category
-
+    labels,
   };
   return proj;
 }
 
 function getProjects() {
   return gProjects;
+}
+
+function getProjById(projId) {
+  var project = gProjects.find(function (proj) {
+    return proj.id === projId
+  })
+  return project
 }
